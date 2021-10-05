2502273809245f9a914f67c86264586b7af1fb12
Lars Jørgensen <lars.b.jn@gmail.com>
2021-10-05 22:59:43 +0200

    Added lecture four exercise

---
+++ Lec4/TicTacToe/TicTacToe/Models/FieldState.swift
@@ -0,0 +1,12 @@
+//
+//  FieldState.swift
+//  TicTacToe
+//
+//  Created by Lars Jørgensen on 05/10/2021.
+//
+
+import Foundation
+
+enum FieldState {
+    case empty, cross, nought
+}
---
+++ Lec4/TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Lars Jørgensen on 05/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
